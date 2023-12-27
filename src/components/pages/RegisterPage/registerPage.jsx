8b1cce97f72ea2ca235c7cc84c126ef3268afe62
rooddeeo <rooddeeo@gmail.com>
2023-12-27 21:38:28 +0200

    React Home Work 8

--- src/components/pages/RegisterPage/registerPage.jsx
+++ src/components/pages/RegisterPage/registerPage.jsx
@@ -1,16 +1,24 @@
 // import css from './registerPage.module.css';
 
 import RegisterForm from 'components/RegisterForm/RegisterForm';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { registerThank } from 'store/authorization/thank';
 
 const RegisterPage = () => {
+  const isAuthorization = useSelector(state => state.authorization.token);
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
 
   const register = user => {
     dispatch(registerThank(user));
-    console.log(user);
   };
+
+  useEffect(() => {
+    isAuthorization && navigate('/');
+  }, [isAuthorization, navigate]);
 
   return <RegisterForm register={register} />;
 };
--- src/store/contacts/operations.js
+++ src/store/contacts/operations.js
@@ -19,7 +19,7 @@ export const addContactNew = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      const response = await axios.post(`/users/signup`, { ...contact });
+      const response = await axios.post(`/contacts`, { ...contact });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +31,7 @@ export const contactDelete = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/contacts/${contactId}`);
+      const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
