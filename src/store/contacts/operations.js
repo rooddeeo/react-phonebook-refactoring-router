6ad29021a7522a1e79698f71ed108bf740ae645d
rooddeeo <rooddeeo@gmail.com>
2023-12-31 02:54:50 +0200

    React Home Work 8

--- src/store/contacts/operations.js
+++ src/store/contacts/operations.js
@@ -6,7 +6,6 @@ export const fetchContacts = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const { data } = await api.get(`/contacts`);
-      console.log(data);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
